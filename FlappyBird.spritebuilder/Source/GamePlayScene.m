5b744e40e54a755996b4b8947dd9b2ab811442f3
YAHACODE <yahya@yahyas-MacBook-Air.local>
2014-06-27 14:48:34 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -4,23 +4,36 @@
 
 @implementation GamePlayScene
 
-character
-CCPhysicsNode
-
 - (void)initialize
-[self addObstacle];
-
-{character = (Character*)[CCBReader load:@"Character"];
-    [physicsNode addChild:character];}
+{
+    // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
+    [self addObstacle];
+    timeSinceObstacle = 0.0f;
+}
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    // Increment the time since the last obstacle was added
+    timeSinceObstacle += delta; // delta is approximately 1/60th of a second
+    
+    // Check to see if two seconds have passed
+    if (timeSinceObstacle > 2.0f)
+    {
+        // Add a new obstacle
+        [self addObstacle];
+        
+        // Then reset the timer.
+        timeSinceObstacle = 0.0f;
+    }
 }
 
-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+// put new methods here
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
     [character flap];
-    (type I return)nameOfMethod: (type of first parameter) firstParameter continuationOfMethodName: (type of second parameter) secondParameter
 }
-end
+
+@end
